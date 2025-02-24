567e2d84c6557c3cff646e0b9923c4fb703bb3be
ConjuringFire <bnthompson87@icloud.com>
2025-02-24 13:23:18 +1000

    created contact model created a migration script for creating contacts

---
+++ app/Models/Contact.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Contact extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'phone', 'email'];
+    
+    public static function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'phone' => 'required|string|regex:/^\+[1-9]\d{7,14}$/',
+            'email' => 'required|email',
+        ];
+    }
+}
